7f6a78992dcf9a20288e8511a53b5a67bf9ef4ff
ShanPrabod <165180388+ShanPrabod@users.noreply.github.com>
2024-05-20 20:47:24 +0530

    Dash Board Design
    Completed Dash Board Design

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('includes/header.php'); ?>
+
+<div class="py-5">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+
+                     <h1 class="display-1">NXTgen POS</h1>
+
+                    <a href="login.php" class="btn btn-primary">login</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
